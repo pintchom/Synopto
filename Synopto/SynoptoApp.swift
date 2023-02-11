54c9d58695b91f4f34224902fc6387b7ea5d32e5
pintchom <maxpintchouk4321@gmail.com>
2023-02-11 15:53:39 -0500

    Initial Commit

---
+++ Synopto/SynoptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FuckTest4App.swift
+//  FuckTest4
+//
+//  Created by Max Pintchouk on 2/5/23.
+//
+
+import SwiftUI
+
+@main
+struct SynoptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
